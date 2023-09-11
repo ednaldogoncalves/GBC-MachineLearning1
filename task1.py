4cddcbeaf8e600ff44a2f8a37c8ea9f4e535c9c4
Ednaldo Gonçalves <ednaldo.tic@gmail.com>
2023-09-11 15:42:46 -0400

    Update task1.py

--- task1.py
+++ task1.py
@@ -55,9 +55,11 @@ def save_file(text):
 	print(name)
 	fname = f'scraped_articles/{name}.txt'
 	# Code here - write a file using with (2 lines)
-	with open(fname, 'w') as fp:
+	
+    with open(fname, 'w') as fp:
 		fp.write(text)
-	# Code ends here
+	
+    # Code ends here
 
 	print(f'File saved in directory {fname}')
 
@@ -66,5 +68,6 @@ if __name__ == '__main__':
 	text = collect_text(get_page())
 	text = clean(text)
 	save_file(text)
+    
 	# Instructions to Run this python code
 	# Give url as https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
